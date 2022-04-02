16c3d62d62e86d215da38cd7dc4d401a00d2115b
Lucas Vohr <lucas.vohr@gmx.net>
2022-04-02 15:59:01 +0200

    Aufgabe vom 02.04.2022

---
+++ Übung_5_1_album.java
@@ -0,0 +1,60 @@
+public class Übung_5_1_album {
+    
+    private String interpret;
+    private String album_titel;
+    private String medium;
+    private int release;
+    private int num_tracks;
+
+    public Übung_5_1_album(String in_interpret, String in_titel, String in_medium, int in_release, int in_num_tracks){
+
+        interpret = in_interpret;
+        album_titel = in_titel;
+        medium = in_medium;
+        release = in_release;
+        num_tracks = in_num_tracks;
+
+    } 
+
+    public void set_interpret(String input){
+            
+        interpret = input;
+
+    }
+
+    public void set_titel(String input){
+            
+        album_titel = input;
+
+    }
+
+    public void set_medium(String input){
+            
+        medium = input;
+
+    }
+
+    public void set_release(int input){
+            
+        release = input;
+
+    }
+
+    public void set_num_tracks(int input){
+            
+        num_tracks = input;
+
+    }
+
+    public void ausgabe(){
+
+        System.out.println("Interpret: \t\t" + interpret);
+        System.out.println("Album: \t\t\t" + album_titel);
+        System.out.println("Medium: \t\t" + medium);
+        System.out.println("Veröffentlichung: \t" + release);
+        System.out.println("Tracks: \t\t" + num_tracks);
+        System.out.println("-------------------------------------------");
+
+    }
+
+}
---
+++ Übung_5_1_main.java
@@ -0,0 +1,20 @@
+public class Übung_5_1_main {
+    
+    public static void main(String[] args){
+
+            System.out.println("-------------------------------------------");
+
+        // Objekt anlegen und erste Ausgabe
+            Übung_5_1_album testalbum = new Übung_5_1_album("Prinz Pi", "Kompass ohne Norden", "CD", 2013, 15);
+            testalbum.ausgabe();
+            
+        // Testweise die Einträge ändern:
+            testalbum.set_interpret("Edgar Wasser");
+            testalbum.set_medium("Stream (Spotify)");
+            testalbum.set_titel("wtf irl");
+            testalbum.set_release(2022);
+            testalbum.ausgabe();
+            
+    }
+
+}
