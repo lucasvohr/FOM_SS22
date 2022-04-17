356a53d672cd04b7e474f7c790e14aff38555bad
Lucas Vohr <lucas.vohr@gmx.net>
2022-04-17 18:59:06 +0200

    Update

---
+++ Uebung_6_Badminton.java
@@ -0,0 +1,12 @@
+public class Uebung_6_Badminton extends Uebung_6_Sportart{
+    
+    public int netzhoehe;
+
+    public Uebung_6_Badminton(String in_name, int in_netzhoehe){
+
+        super(in_name);
+        netzhoehe = in_netzhoehe;
+
+    }
+
+}
--- Uebung_6_Sportart.java
+++ Uebung_6_Sportart.java
@@ -1,9 +1,9 @@
-public class Übung_6_Sportart {
+public class Uebung_6_Sportart {
     
     // "protected", damit in der Unterklasse auf dieses Feld zugegriffen werden kann
         protected String name;
 
-        public Übung_6_Sportart(String in_name){
+        public Uebung_6_Sportart(String in_name){
             name = in_name;
         }
 
